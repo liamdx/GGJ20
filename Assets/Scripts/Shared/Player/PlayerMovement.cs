eb755b53ecd2acd755d564782da76a84f00890ef
liam <liam_devlin@live.co.uk>
2020-01-31 20:59:19 +0000

    playermovement added framework

---
+++ Assets/Scripts/Shared/Player/PlayerMovement.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float m_Speed;
+    public float m_Acceleration;
+    public float m_TurnSpeed;
+    public float m_CanMove;
+}
