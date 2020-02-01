afaa06abda4d3259d36885c6881bba893900e59d
liam <liam_devlin@live.co.uk>
2020-02-01 18:01:15 +0000

    Camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class CameraController : MonoBehaviour
 {
     public PlayerMovement[] m_Targs;
@@ -9,11 +10,17 @@ public class CameraController : MonoBehaviour
     private Camera Par_Camera;
 
     public Vector3 offset;
+    private Vector3 velocity;
+    public float smoothTime= 0.5f;
+
+    public float minZoom=40f;
+    public float maxZoom=10f;
+    public float ZoomLim=50f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Par_Camera=GetComponent<Camera>();
         m_Targs= FindObjectsOfType<PlayerMovement>();
 
     }
@@ -23,15 +30,44 @@ public class CameraController : MonoBehaviour
     {
         if (m_Targs.Length==0)
         {return;}
+
+        move();
+        zoom();
+
+
+    }
+
+
+    void move()
+    {
         Vector3 TargPos=GetTargetPosition();
         Vector3 newposition=TargPos+offset;
 
         Debug.DrawRay(TargPos, Vector3.up);
         
-        gameObject.transform.position = newposition;
+        gameObject.transform.position = Vector3.SmoothDamp(transform.position , newposition , ref velocity, smoothTime) ;
         gameObject.transform.LookAt(TargPos);
-
     }
+
+    void zoom()
+    {
+        float Zoomer=Mathf.Lerp(maxZoom,minZoom, getGreatestDistance()/ZoomLim);
+        Par_Camera.fieldOfView=Mathf.Lerp(Par_Camera.fieldOfView, Zoomer, Time.deltaTime);
+    }
+
+
+    float getGreatestDistance()
+    {
+        var bounds = new Bounds(m_Targs[0].transform.position,Vector3.zero);
+        
+        foreach (PlayerMovement pos in m_Targs)
+        {
+            bounds.Encapsulate(pos.transform.position);
+        }
+
+        return bounds.size.z+bounds.size.x;
+    }
+
 
     Vector3 GetTargetPosition()
     {
---
+++ Assets/Widgets/MainMenuScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MainMenuScript : MonoBehaviour
+{
+    public GameObject Targ;
+    public int MenuSwitch=0;
+
+    public void PlayPressed()
+    {
+        MenuSwitch=1;
+        Debug.Log("PlayPressed");
+    }
+
+    public void OptionsPressed()
+    {
+        MenuSwitch=2;
+        Debug.Log("OptionsPressed");
+    }
+
+    public void BackPressed()
+    {
+        MenuSwitch=0;
+        Debug.Log("BackPressed");
+    }
+
+    public void ExitPressed()
+    {
+        MenuSwitch=4;
+        Debug.Log("EXIT");
+    }
+
+}
